1a0f630a2cbbe105894e916733bac3c0ff65c44f
chaos23 <mik342003@gmail.com>
2025-05-04 20:45:07 +0200

    Rewritten Test Burn Asset

--- tests/type06Test.py
+++ tests/type06Test.py
@@ -3,25 +3,40 @@ from tests.helpers import Helpers
 import pywaves as pw
 from pywaves import address
 from pywaves import asset
+import time
+from random import randint
 
 PYWAVES_TEST_NODE = os.getenv('PYWAVES_TEST_NODE')
-PYWAVES_TEST_PRIVATEKEY = os.getenv('PYWAVES_TEST_PRIVATEKEY')
+PYWAVES_FAUCET_PRIVATEKEY = os.getenv('PYWAVES_TEST_SECRET')
 PYWAVES_TEST_ASSET = os.getenv('PYWAVES_TEST_ASSET')
 
 pw.setThrowOnError(True)
 
+pw.setNode(PYWAVES_TEST_NODE, 'T')
+faucet = address.Address(privateKey=PYWAVES_FAUCET_PRIVATEKEY)
+print(f"Operating on address: {faucet.address}")
+balance = faucet.balance()
+if (balance < 100000000):
+    raise Exception('Faucet balance is too low. Please refill it.')
+
 def test_succesfullBurnAsset():
     helpers = Helpers()
-    pw.setNode(PYWAVES_TEST_NODE, 'T')
-    myAddress = address.Address(privateKey=PYWAVES_TEST_PRIVATEKEY)
-    print(f"Operating on address: {myAddress.address}")
-    myToken = asset.Asset(PYWAVES_TEST_ASSET)
-
-    txId = myAddress.burnAsset(myToken, 1)
+    tokenQuantity = randint(1000000, 99999999)
+    tokenDecimals = randint(0, 8)
+    tokenName = "PW_" + str(tokenQuantity) + str(tokenDecimals)
+    print(f"Creating asset {tokenName} with quantity {tokenQuantity} and decimal {tokenDecimals}")
+    token = faucet.issueAsset(tokenName, f"Test Token {tokenName}", tokenQuantity, tokenDecimals, reissuable=False)
+    while not token.status():
+	    pass
+    tokenId = token.getId()
+    print(f"Token {tokenName} created with ID: {tokenId}")
+    txId = faucet.burnAsset(token, 1)
     blockchaintx = helpers.waitFor(txId)
-
+    
     assert blockchaintx['id'] == txId
 
+
+'''
 def test_pywavesOfflineBurnAsset():
     pw.setNode(PYWAVES_TEST_NODE, 'T')
     myAddress = address.Address(privateKey=PYWAVES_TEST_PRIVATEKEY)
@@ -32,3 +47,4 @@ def test_pywavesOfflineBurnAsset():
     pw.setOnline()
 
     assert tx['api-type'] == 'POST'
+'''
