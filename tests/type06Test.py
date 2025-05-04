dd738426db9ee0ac362e7a6461c59440d029f415
chaos23 <mik342003@gmail.com>
2025-05-04 13:00:00 +0200

    Converted test to use secrets and variables

--- tests/type06Test.py
+++ tests/type06Test.py
@@ -1,15 +1,21 @@
+import os
 from tests.helpers import Helpers
 import pywaves as pw
 from pywaves import address
 from pywaves import asset
 
+PYWAVES_TEST_NODE = os.getenv('PYWAVES_TEST_NODE')
+PYWAVES_TEST_PRIVATEKEY = os.getenv('PYWAVES_TEST_PRIVATEKEY')
+PYWAVES_TEST_ASSET = os.getenv('PYWAVES_TEST_ASSET')
+
 pw.setThrowOnError(True)
 
 def test_succesfullBurnAsset():
     helpers = Helpers()
-    pw.setNode('https://nodes-testnet.wavesnodes.com', 'T')
-    myAddress = address.Address(privateKey='BGpBRDeUiHskf4bdyWoUAKpP9DSx51haovHcGNqPEy6Q')
-    myToken = asset.Asset('226sJnCCEeQXushTBPtwyWY8id44ejHdvET1b9ESSAqy')
+    pw.setNode(PYWAVES_TEST_NODE, 'T')
+    myAddress = address.Address(privateKey=PYWAVES_TEST_PRIVATEKEY)
+    print(f"Operating on address: {myAddress.address}")
+    myToken = asset.Asset(PYWAVES_TEST_ASSET)
 
     txId = myAddress.burnAsset(myToken, 1)
     blockchaintx = helpers.waitFor(txId)
@@ -17,9 +23,10 @@ def test_succesfullBurnAsset():
     assert blockchaintx['id'] == txId
 
 def test_pywavesOfflineBurnAsset():
-    pw.setNode('https://nodes-testnet.wavesnodes.com', 'T')
-    myAddress = address.Address(privateKey='BGpBRDeUiHskf4bdyWoUAKpP9DSx51haovHcGNqPEy6Q')
-    myToken = asset.Asset('226sJnCCEeQXushTBPtwyWY8id44ejHdvET1b9ESSAqy')
+    pw.setNode(PYWAVES_TEST_NODE, 'T')
+    myAddress = address.Address(privateKey=PYWAVES_TEST_PRIVATEKEY)
+    print(f"Operating on address: {myAddress.address}")
+    myToken = asset.Asset(PYWAVES_TEST_ASSET)
     pw.setOffline()
     tx = myAddress.burnAsset(myToken, 1)
     pw.setOnline()
