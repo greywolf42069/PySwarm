a954b871a1d5a284894d781251b6fb89daefa708
chaos23 <mik342003@gmail.com>
2025-05-05 10:53:47 +0200

    Rewritten Test 06

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,8 +2,10 @@ import os
 import pywaves as pw
 import requests
 import time
+from pywaves import address
 
 PYWAVES_TEST_NODE = os.getenv('PYWAVES_TEST_NODE')
+PYWAVES_FAUCET_SECRET = os.getenv('PYWAVES_TEST_SECRET')
 
 class Helpers:
 
@@ -18,3 +20,19 @@ class Helpers:
             return self.waitFor(id)
         else:
             return response
+
+    def prepareTestcase(self, testcase):
+        pw.setNode(PYWAVES_TEST_NODE, 'T')
+        faucet = address.Address(privateKey=PYWAVES_FAUCET_SECRET)
+        balance = faucet.balance()
+        if (balance < 100000000):
+            raise Exception('Faucet balance is too low. Please refill it.')
+
+        testwallet = address.Address(seed=PYWAVES_FAUCET_SECRET+testcase)
+        testwalletBalance = testwallet.balance()
+        if (testwalletBalance < 100000000):
+            faucet.sendWaves(testwallet, 100000000-testwalletbalance)
+        print(f"Operating on address: {testwallet.address}")
+        return testwallet
+
+
--- tests/type06Test.py
+++ tests/type06Test.py
@@ -1,48 +1,41 @@
-import os
-from tests.helpers import Helpers
 import pywaves as pw
 from pywaves import address
 from pywaves import asset
 import time
 from random import randint
+from tests.helpers import Helpers
+import os
+
 
 PYWAVES_TEST_NODE = os.getenv('PYWAVES_TEST_NODE')
-PYWAVES_FAUCET_PRIVATEKEY = os.getenv('PYWAVES_TEST_SECRET')
-PYWAVES_TEST_ASSET = os.getenv('PYWAVES_TEST_ASSET')
+THISTEST = 'test06'
 
 pw.setThrowOnError(True)
-
 pw.setNode(PYWAVES_TEST_NODE, 'T')
-faucet = address.Address(privateKey=PYWAVES_FAUCET_PRIVATEKEY)
-print(f"Operating on address: {faucet.address}")
-balance = faucet.balance()
-if (balance < 100000000):
-    raise Exception('Faucet balance is too low. Please refill it.')
 
-def test_succesfullBurnAsset():
-    helpers = Helpers()
-    tokenQuantity = randint(1000000, 99999999)
-    tokenDecimals = randint(0, 8)
-    tokenName = "PW_" + str(tokenQuantity) + str(tokenDecimals)
-    print(f"Creating asset {tokenName} with quantity {tokenQuantity} and decimal {tokenDecimals}")
-    token = faucet.issueAsset(tokenName, f"Test Token {tokenName}", tokenQuantity, tokenDecimals, reissuable=False)
+helpers = Helpers()
+testwallet = helpers.prepareTestcase(THISTEST)
+
+assets = testwallet.assets()
+
+if len(assets) == 0:
+    token = testwallet.issueAsset('PW_'+THISTEST, f"Test Token {THISTEST}", 10000000, 8, reissuable=False)
     while not token.status():
 	    pass
-    txId = faucet.burnAsset(token, 1)
+else:
+    token = asset.Asset(assets[0])
+
+print("Using token: ", token)
+
+def test_succesfullBurnAsset():
+    txId = testwallet.burnAsset(token, 1)
     blockchaintx = helpers.waitFor(txId)
     
     assert blockchaintx['id'] == txId
 
-
-'''
 def test_pywavesOfflineBurnAsset():
-    pw.setNode(PYWAVES_TEST_NODE, 'T')
-    myAddress = address.Address(privateKey=PYWAVES_TEST_PRIVATEKEY)
-    print(f"Operating on address: {myAddress.address}")
-    myToken = asset.Asset(PYWAVES_TEST_ASSET)
     pw.setOffline()
-    tx = myAddress.burnAsset(myToken, 1)
+    tx = testwallet.burnAsset(token, 1)
     pw.setOnline()
 
     assert tx['api-type'] == 'POST'
-'''
