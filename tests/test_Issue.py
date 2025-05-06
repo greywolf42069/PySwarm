71583f894dd0afb1aff68e4155daa1f7568bfdf8
Sunchaser23 <qflcpt@protonmail.com>
2025-05-06 17:54:04 +0200

    Added tests of type03Test in test_Issue

--- tests/test_Issue.py
+++ tests/test_Issue.py
@@ -4,6 +4,7 @@ from pywaves import asset
 import time
 from tests.helpers import Helpers
 import os
+import pytest
 
 PYWAVES_FAUCET_SECRET = os.getenv('PYWAVES_TEST_SECRET')
 PYWAVES_TEST_NODE = os.getenv('PYWAVES_TEST_NODE')
@@ -19,10 +20,36 @@ faucet = address.Address(privateKey=PYWAVES_FAUCET_SECRET)
 testwallet = helpers.prepareTestcase()
 
 # add an extra 1 wves funding to the testwallet
-tx = faucet.sendWaves(testwallet, 100000000)
+tx = faucet.sendWaves(testwallet, 200000000)
 helpers.waitFor(tx['id'])
 
 try:
+
+    def test_issueAssetWithoutPrivateKey():
+        with pytest.raises(Exception) as error:
+            myAdress = address.Address('3MwGH6GPcq7jiGNXgS4K6buynpLZR5LAgQm')
+            myAdress.issueAsset('Test2','This is just another test asset', 100000, 1)
+
+        assert str(error) == '<ExceptionInfo PyWavesException(\'Private key required\') tblen=3>'
+
+    def test_issueAssetWithTooShortName():
+        with pytest.raises(Exception) as error:
+            testwallet.issueAsset('Tes','This is just another test asset', 100000, 1)
+
+        assert str(error) == '<ExceptionInfo PyWavesException(\'Asset name must be between 4 and 16 characters long\') tblen=3>'
+
+    def test_pywavesOffline():
+        pw.setOffline()
+        tx = testwallet.issueAsset('Test2', 'this is just another test asset', 10000, 0)
+        pw.setOnline()
+
+        assert tx['api-type'] == 'POST'
+
+    def test_issueAssetWithTooLongName():
+        with pytest.raises(Exception) as error:
+            testwallet.issueAsset('12345678912345678','This is just another test asset', 100000, 1)
+
+        assert str(error) == '<ExceptionInfo PyWavesException(\'Asset name must be between 4 and 16 characters long\') tblen=3>'
 
     def test_successfulIssueAsset():
         token = testwallet.issueAsset(NAME, f"Test Token {NAME}", 100, 8, reissuable=True)
