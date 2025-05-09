7bf5e7a333d84e6fb783fa0c9ad41e353d52646d
Sunchaser23 <qflcpt@protonmail.com>
2025-05-09 18:11:44 +0200

    Added Test Cases

--- PYTHON_TESTS.py
+++ PYTHON_TESTS.py
@@ -3,13 +3,13 @@
 PYTHON_TESTS = [
     "tests/test_BurnAsset.py",
     "tests/test_CancelLease.py",
-    # removed for the time being to save waves
-    #"tests/test_IssueAsset.py",
+    "tests/test_IssueAsset.py",
     "tests/test_Lease.py",
     "tests/test_ReissueAsset.py",
     "tests/test_SendAsset.py",
     "tests/test_SendWaves.py",
-    # removed for the time being to save waves
-    #"tests/test_SmartAsset.py",
+    "tests/test_SmartAsset.py",
     "tests/test_Alias.py",
+    "tests/test_MassTransferAsset.py",
+    "tests/test_MassTransferWaves.py",
 ]
