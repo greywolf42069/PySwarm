20376a577d70a0eb692decfe4d4b394e99d23e16
Sunchaser23 <qflcpt@protonmail.com>
2025-05-10 18:56:40 +0200

    WIP

--- PYTHON_TESTS.py
+++ PYTHON_TESTS.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 PYTHON_TESTS = [
+    "tests/test_Alias.py",
     "tests/test_BurnAsset.py",
     "tests/test_CancelLease.py",
     "tests/test_IssueAsset.py",
+    "tests/test_IssueSmartAsset.py",
     "tests/test_Lease.py",
+    "tests/test_MassTransferAsset.py",
+    "tests/test_MassTransferWaves.py",
     "tests/test_ReissueAsset.py",
     "tests/test_SendAsset.py",
     "tests/test_SendWaves.py",
-    "tests/test_SmartAsset.py",
-    "tests/test_Alias.py",
-    "tests/test_MassTransferAsset.py",
-    "tests/test_MassTransferWaves.py",
+    "tests/test_SetAssetScript.py",
+    "tests/test_SetScript.py",
+    "tests/test_SponsorAsset.py",
 ]
